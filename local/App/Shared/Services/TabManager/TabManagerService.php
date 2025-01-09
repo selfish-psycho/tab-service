9f9fe0113850a958b6a8a3bec1a1b26d8839f4d5
Alexandr Knyazev <asknyazev2402@gmail.com>
2025-01-09 20:16:31 +0500

    Сам сервис

---
+++ local/App/Shared/Services/TabManager/TabManagerService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Shared\Services\TabManager;
+
+use App\Shared\Contracts\BaseServiceInterface;
+use App\Shared\Contracts\Tabs\ServiceInterface;
+use App\Shared\DI\Container;
+use App\Shared\Enums\TabManager\ServicesEnums;
+use App\Shared\Services\TabManager\BaseTab\BaseTabService;
+
+class TabManagerService implements BaseServiceInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public static function create(int $typeId = ServicesEnums::BASE->value): ServiceInterface
+    {
+        return match($typeId) {
+            ServicesEnums::BASE->value => (new Container())->get(BaseTabService::class),
+        };
+    }
+}
