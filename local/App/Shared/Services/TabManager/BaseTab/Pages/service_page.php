421089060c07c5545ed9eb36deb6635606497374
Alexandr Knyazev <asknyazev2402@gmail.com>
2025-01-09 20:12:32 +0500

    Страница инициализации компонента таба

---
+++ local/App/Shared/Services/TabManager/BaseTab/Pages/service_page.php
@@ -0,0 +1,23 @@
+<?php
+
+use Bitrix\Main\Application;
+
+require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
+
+//$app = Application::getInstance()->getContext();
+$app = new CMain();
+
+$app->ShowHead();
+
+$requestParams = Application::getInstance()->getContext()->getRequest()->get('PARAMS');
+$componentName = $requestParams['component_name'];
+$componentTemplate = $requestParams['component_template'];
+
+$app->IncludeComponent(
+    componentName: $componentName,
+    componentTemplate: $componentTemplate,
+    arParams: [
+        'ENTITY_ID' => $requestParams['entity_id'],
+        'ENTITY_TYPE_ID' => $requestParams['entity_type_id']
+    ]
+);
